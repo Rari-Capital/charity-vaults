1c24707cd6339860493670cbe294737e68a2b622
Andreas Bigger <abigger@nascent.xyz>
2021-09-12 19:44:21 -0700

    :construction_worker: ran through a bunch of logic with Nico :fire:

--- README.md
+++ README.md
@@ -1,11 +1,29 @@
-# charity-vaults
+# Charity Vaults
 
 Share interest from Fuse Vaults with charities, friends, and more.
 
-## Getting Started
+### Getting Started
 
 ```sh
 git clone https://github.com/Rari-Capital/charity-vaults.git
 cd charity-vaults
 make
 ```
+
+
+### Questions
+
+Are users able to permissionlessly create Vaults?
+
+How do we envision deposits in a Vault from a ui perspective?
+Say I deposit usdc with one referral link for Charity A with a gift rate of 10% of any interest earned.
+Then I deposit usdc with a different referral link for Charity B with a gift rate of 20% of any interest earned.
+Will the I see a list of the deposits on the UI with each respective interest earned, amount, and gift rate + charity? And I basically have the ability to withdraw x amount from each deposit in one transaction?
+
+Should referrals have the option to choose the underlying asset or does the charity automatically take any donations?
+
+Should referrals have the gift rate optionally set by the charity or it should always be up to the user depositing?
+(We _could_ always just not have the ui for the charity to set the rate for a given referral and always allow the user to pass in a gift rate and we verify that the charity didn't already set the rate manually and in that case revert the deposit tx)
+
+Is anybody able to be a charity - like is adding a charity permissionless?
+
--- src/CharityVaults.sol
+++ src/CharityVaults.sol
@@ -9,31 +9,54 @@ import "vaults/VaultFactory.sol";
 /// @author Transmissions11, JetJadeja, some_random_usc_kids
 /// @notice Charity wrapper for vaults/VaultFactory.
 contract CharityVaults {
+    /// @dev This factory is already deployed
+    VaultFactory public factory;
 
-    VaultFactory public factory = new VaultFactory();
-
-    /// @notice Creates a CharityVault 
-    constructor() {
+    /// @notice Creates a CharityVault
+    constructor(address _deployed_vault_factory) {
         charities = new Charity[]();
+        factory = new VaultFactory(_deployed_vault_factory);
     }
 
     /// TODO: Internal store to track user-specified rates
     /// TODO: Map referals to their Charity of choice
 
     /// @notice users can send percents to any address (ex endaoment address)
-    
+
     /// TODO: interest is sent on withdraw
     /// TODO: only the carity vault donation recipient can call the withdraw function to withdraw their interest
 
+    /// @notice Referral
+    struct Referral {
+        uint256 id;
+        uint256 charity_id;
+        // TODO: is this optional - should the charity be able to set the gift_percent at all?
+        bool is_gift_percent_set;
+        uint256 gift_percent;
+    }
 
-    /// @notice Charity Endpoint
+    /// @notice mapping from referral id to a Referral Object
+    mapping(uint256 => Referral) public referrals;
+
+    /// @notice Charity
     struct Charity {
+        uint256 charity_id;
         string name;
         address donation_address;
+        address[] approved_referral_creators;
         /// TODO: add assets in the Charity Vault - then we swap through uniswap v3
-        
     }
-    
+
+    modifier approvedCharityCreators(Charity charity) {
+        require(charity.approved_referral_creators.contains(msg.sender), "msg.sender is not approved for this charity!");
+        _;
+    }
+
+    // TODO: add Referral Function
+    // TODO: Delete Referral Function
+    // TODO: *If* the charity is able to set the gift rate, are they able to change the rate for a given referral?
+        // TODO: then, we would need edit referral logic
+
     /// @notice A list of approved charities
     Charity[] private charities;
 
@@ -49,6 +72,9 @@ contract CharityVaults {
     /// @param charity The Charity Object removed
     event CharityRemoved(Charity charity);
 
+    /// @notice Emitted after a Charity is removed from the list of approved charities
+    /// @param charity The Charity Object removed
+    event CharityEdited(Charity charity);
 
     /// @notice Allows a given owner to add to the list of approved charities
     /// @param charity The new Charity Object to be added
@@ -66,14 +92,35 @@ contract CharityVaults {
         emit CharityRemoved(charity);
     }
 
+
+    // TODO: Edit Charity Function - for changing name and donation_address
+
+
     /// @notice Deposit the vault's underlying token to mint fvTokens.
     /// @param underlyingAmount The amount of the underlying token to deposit.
     /// @param underlying The underlying ERC20 token the Vault earns yield on.
-    function deposit(uint256 underlyingAmount, ERC20 underlying) external {
+    function deposit(
+        uint256 underlyingAmount,
+        ERC20 underlying,
+        uint256 referral_id
+    ) external {
         // Get the respective Vault
         Vault vault = factory.getVaultFromUnderlying(underlying);
 
-        // TODO: determine charity rate mechanics
+        // TODO: this function should take a list of deposits for various vaults/underlying assets,
+
+        // Fetch the Referral Object from the provided referral_id
+        Referral referral = referrals[referral_id];
+
+        max_deposit_id += 1;
+        uint256 deposit_id = max_deposit_id;
+        deposits[deposit_id] = new Deposit(
+            deposit_id,
+            vault,
+            underlyingAmount,
+            referral
+        );
+        user_deposits.push(deposit_id);
 
         // Relay deposit to the respective vault
         vault.deposit(underlyingAmount);
@@ -81,11 +128,20 @@ contract CharityVaults {
 
     /// @notice Burns fvTokens and sends underlying tokens to the caller.
     /// @param amount The amount of fvTokens to redeem for underlying tokens.
-    function withdraw(uint256 amount, ERC20 underlying) external {
+    function withdraw(
+        uint256 amount,
+        ERC20 underlying,
+        uint256 deposit_id
+    ) external {
         // Get the respective Vault
         Vault vault = factory.getVaultFromUnderlying(underlying);
 
-        // TODO: determine charity rate withdraw mechanics 
+        // TODO: this function should take a list of withdraws for various vaults/underlying assets,
+
+        // Fetch the Deposit Object from the provided deposit_id
+        Deposit deposit = deposits[deposit_id];
+
+        // TODO: determine charity rate withdraw mechanics
 
         // Relay withdraw to the respective vault
         vault.withdraw(amount);
@@ -95,7 +151,11 @@ contract CharityVaults {
     /// @param user A given EOA.
     /// @param underlying The underlying ERC20 token the Vault earns yield on.
     /// @return uint256 The balance of the user for the specified vault
-    function getVaultBalance(address user, ERC20 underlying) external view returns (uint256) {
+    function getVaultBalance(address user, ERC20 underlying)
+        external
+        view
+        returns (uint256)
+    {
         // Get the respective Vault
         Vault vault = factory.getVaultFromUnderlying(underlying);
 
