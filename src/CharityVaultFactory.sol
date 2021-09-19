fe7ec7c18f6a5215d4a36f04ecf85f29bb89f231
Andreas Bigger <abigger87@gmail.com>
2021-09-19 11:00:05 -0700

    basic charity vault factory implementation

--- src/CharityVaultFactory.sol
+++ src/CharityVaultFactory.sol
@@ -11,6 +11,12 @@ import "./CharityVault.sol";
 contract CharityVaultFactory {
     using Bytes32AddressLib for *;
 
+    /// @dev Maps bytes32 hash of underlying erc20 token + charity donation address + fee percent to a CharityVault
+    mapping(bytes32 => address) public cvaults;
+    mapping(bytes32 => bool) public cvaultExists;
+
+
+
     /*///////////////////////////////////////////////////////////////
                                  EVENTS
     //////////////////////////////////////////////////////////////*/
@@ -31,32 +37,26 @@ contract CharityVaultFactory {
     /// @param feePercent percent of earned interest sent to the charity as a donation
     /// @return cvault The newly deployed CharityVault contract.
     function deployCharityVault(ERC20 underlying, address payable charity, uint256 feePercent) external returns (CharityVault cvault) {
-        // Compute the create2 hash.
-        bytes32 create2Hash = keccak256(
+        // Compute a CharityVault Hash
+        // TODO: how to compute the hash of the parameters
+        bytes32 cvaultHash = keccak256(
             abi.encodePacked(
-                // Prefix:
-                bytes1(0xFF),
-                // Creator:
-                address(this),
-                // Salt:
-                address(underlying).toBytes32(),
-                // Bytecode hash:
-                keccak256(
-                    abi.encodePacked(
-                        // Deployment bytecode:
-                        type(Vault).creationCode,
-                        // Constructor arguments:
-                        abi.encode(underlying)
-                    )
-                )
+                address(underlying),
+                charity,
+                feePercent
             )
         );
+
+        require(cvaultExists[cvaultHash], "Charity Vault already exists!");
         
-        // Use the create2 opcode to deploy a Vault contract.
+        // Use the create2 opcode to deploy a CharityVault contract.
         // This will revert if a vault with this underlying has already been 
         // deployed, as the salt would be the same and we can't deploy with it twice.
-        cvault = new Vault{salt: create2Hash}(underlying);
+        cvault = new CharityVault{salt: cvaultHash}(underlying, charity, feePercent);
 
+        // Map the parameters to our new CharityVault
+        cvaults[cvaultHash] = address(cvault);
+        cvaultExists[cvaultHash] = true;
         emit CharityVaultDeployed(underlying, cvault);
     }
 
@@ -67,31 +67,22 @@ contract CharityVaultFactory {
     /// @notice Computes a CharityVault's address from its underlying token, donation address, and fee percent.
     /// @dev The CharityVault returned may not have been deployed yet.
     /// @param underlying The underlying ERC20 token the CharityVault earns yield on.
+    /// @param charity donation address
+    /// @param feePercent percent of earned interest sent to the charity as a donation
     /// @return The CharityVault that supports this underlying token.
-    function getCharityVaultFromUnderlying(ERC20 underlying) external view returns (Vault) {
-        // Compute the create2 hash.
-        bytes32 create2Hash = keccak256(
+    function getCharityVaultFromUnderlying(ERC20 underlying, address payable charity, uint256 feePercent) external view returns (Vault) {
+        // Compute a CharityVault Hash
+        // TODO: how to compute the hash of the parameters
+        bytes32 cvaultHash = keccak256(
             abi.encodePacked(
-                // Prefix:
-                bytes1(0xFF),
-                // Creator:
-                address(this),
-                // Salt:
-                address(underlying).toBytes32(),
-                // Bytecode hash:
-                keccak256(
-                    abi.encodePacked(
-                        // Deployment bytecode:
-                        type(Vault).creationCode,
-                        // Constructor arguments:
-                        abi.encode(underlying)
-                    )
-                )
+                address(underlying),
+                charity,
+                feePercent
             )
         );
 
-        // Convert the create2 hash into a Vault.
-        return Vault(payable(create2Hash.toAddress()));
+        // Fetch the given CharityVault from the hash of it's parameters
+        return CharityVault(payable(cvaults[cvaultHash]));
     }
 
     /// @notice Returns if a charity vault at an address has been deployed yet.
