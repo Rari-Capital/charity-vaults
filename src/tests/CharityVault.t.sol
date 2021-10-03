a9c155f75d78b9ea199b04100019aaef182945aa
Andreas Bigger <abigger87@gmail.com>
2021-10-03 15:56:16 -0700

    :test_tube: writing tests for CharityVault :construction:

--- src/tests/CharityVault.t.sol
+++ src/tests/CharityVault.t.sol
@@ -21,29 +21,41 @@ contract CharityVaultTest is DSTestPlus {
         cvault = new CharityVault(underlying, caddress, cfeePercent, vault);
     }
 
+    /// @notice Tests if we can deploy a charity vault with valid fuzzed parameters
     function test_deploy_charity_vault(address payable _address, uint256 _feePercent) public {
+        // ** We can ignore out of range fee percents
+        if(_feePercent > 100 || _feePercent < 0) return;
+
         CharityVault newVault = new CharityVault(underlying, _address, _feePercent, vault);
         
         // Assert our CharityVault parameters are equal
         assertTrue(address(newVault).code.length > 0);
-        assertEq(caddress, newVault.charity());
-        assertEq(cfeePercent, newVault.feePercent());
-        assertERC20Eq(newVault.underlying(), underlying);
+        // assertEq(caddress, newVault.charity());
+        // assertEq(cfeePercent, newVault.feePercent());
+        // assertERC20Eq(newVault.underlying(), underlying);
+    }
+
+    /// @notice Tests if deployment fails for invalid parameters
+    function testFail_deploy_charity_vault(address payable _address, uint256 _feePercent) public {
+        // ** We should fail for valid feePercents - only testing invalid params
+        assertFalse(_feePercent <= 100 || _feePercent >= 0);
+
+        CharityVault _newVault = new CharityVault(underlying, _address, _feePercent, vault);
     }
 
     /// @notice we shouldn't be able to deploy multiple charity vaults with the same configuration
-    function testFail_deploy_equal_config_cvaults() public {
-        test_deploy_charity_vault(caddress, cfeePercent);
-        test_deploy_charity_vault(caddress, cfeePercent);
-    }
+    // function testFail_deploy_equal_config_cvaults() public {
+    //     test_deploy_charity_vault(caddress, cfeePercent);
+    //     test_deploy_charity_vault(caddress, cfeePercent);
+    // }
 
-    /// @notice we should, however, be able to deploy charity vaults with slightly varying parameters
-    function test_deploys_vaults_varying_params() public {
-        test_deploy_charity_vault(payable(address(0)), 5);
-        test_deploy_charity_vault(payable(address(1)), 5);
-        test_deploy_charity_vault(payable(address(0)), cfeePercent);
-        test_deploy_charity_vault(payable(address(1)), cfeePercent);
-    }
+    // /// @notice we should, however, be able to deploy charity vaults with slightly varying parameters
+    // function test_deploys_vaults_varying_params() public {
+    //     test_deploy_charity_vault(payable(address(0)), 5);
+    //     test_deploy_charity_vault(payable(address(1)), 5);
+    //     test_deploy_charity_vault(payable(address(0)), cfeePercent);
+    //     test_deploy_charity_vault(payable(address(1)), cfeePercent);
+    // }
 
     // function test_basic_charity_vault_not_deployed() public {
     //     assertTrue(
