659ef37e7c3e858975e4f609a4799047bd6ca69c
Andreas Bigger <abigger87@gmail.com>
2021-11-03 21:28:32 -0700

    :rocket: deployed CharityVaultStrategy :rocket:

--- src/tests/mocks/CharityVaultStrategy.sol
+++ src/tests/mocks/CharityVaultStrategy.sol
@@ -1,96 +1,86 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity 0.8.9;
 
-import {ERC20} from "solmate/erc20/ERC20.sol";
-import {SafeERC20} from "solmate/erc20/SafeERC20.sol";
+import {ERC20} from "solmate/tokens/ERC20.sol";
+import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
 import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
 
-import {Strategy} from "vaults/Strategy.sol";
+import {ERC20Strategy} from "vaults/interfaces/Strategy.sol";
 
 /// @title CharityVaultStrategy
 /// @notice This is essentially a malicious strategy that over-reports a user's balance
 contract CharityVaultStrategy is
-    Strategy,
-    ERC20("CV Mock Strategy", "cvsMOCK", 18)
+    ERC20("CV Mock Strategy", "cvsMOCK", 18),
+    ERC20Strategy
 {
-    using SafeERC20 for ERC20;
+    using SafeTransferLib for ERC20;
     using FixedPointMathLib for uint256;
 
-    ERC20 public immutable override underlying;
+    /*///////////////////////////////////////////////////////////////
+                           STRATEGY FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
 
-    uint256 public immutable BASE_UNIT;
+    constructor(ERC20 _UNDERLYING) {
+        UNDERLYING = _UNDERLYING;
 
-    function mint() external payable {}
+        BASE_UNIT = 10**_UNDERLYING.decimals();
+    }
 
-    function isCEther() external pure returns (bool) {
+    function isCEther() external pure override returns (bool) {
         return false;
     }
 
-    constructor(ERC20 _underlying) {
-        underlying = _underlying;
-
-        BASE_UNIT = 10**_underlying.decimals();
+    function underlying() external view override returns (ERC20) {
+        return UNDERLYING;
     }
 
-    function mint(uint256 underlyingAmount)
-        external
-        override
-        returns (uint256)
-    {
-        // Convert underlying tokens to cTokens and mint them.
-        _mint(msg.sender, underlyingAmount.fdiv(exchangeRate(), BASE_UNIT));
+    function mint(uint256 amount) external override returns (uint256) {
+        _mint(msg.sender, amount.fdiv(exchangeRate(), BASE_UNIT));
 
-        // Transfer in underlying tokens from the sender.
-        underlying.safeTransferFrom(
-            msg.sender,
-            address(this),
-            underlyingAmount
-        );
+        UNDERLYING.safeTransferFrom(msg.sender, address(this), amount);
 
         return 0;
     }
 
-    function redeemUnderlying(uint256 underlyingAmount)
-        external
-        override
-        returns (uint256)
-    {
-        // Convert underlying tokens to cTokens and then burn them.
-        _burn(msg.sender, underlyingAmount.fdiv(exchangeRate(), BASE_UNIT));
+    function redeemUnderlying(uint256 amount) external override returns (uint256) {
+        _burn(msg.sender, amount.fdiv(exchangeRate(), BASE_UNIT));
 
         // Calculate a 10% accrual
-        uint256 ten_percent = underlyingAmount.fdiv(
-            underlyingAmount.fmul(10, BASE_UNIT),
+        uint256 ten_percent = amount.fdiv(
+            amount.fmul(10, BASE_UNIT),
             BASE_UNIT
         );
 
-        // Transfer underlying tokens to the caller.
-        underlying.safeTransfer(msg.sender, underlyingAmount + ten_percent);
+        UNDERLYING.safeTransfer(msg.sender, amount + ten_percent);
 
         return 0;
     }
 
-    function simulateLoss(uint256 underlyingAmount) external {
-        underlying.safeTransfer(
-            0x000000000000000000000000000000000000dEaD,
-            underlyingAmount
-        );
+    function balanceOfUnderlying(address user) external view override returns (uint256) {
+        return balanceOf[user].fmul(exchangeRate(), BASE_UNIT);
     }
 
-    function balanceOfUnderlying(address account)
-        external
-        view
-        override
-        returns (uint256)
-    {
-        return balanceOf[account].fmul(exchangeRate(), BASE_UNIT);
-    }
+    /*///////////////////////////////////////////////////////////////
+                             INTERNAL LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    ERC20 internal immutable UNDERLYING;
+
+    uint256 internal immutable BASE_UNIT;
 
     function exchangeRate() internal view returns (uint256) {
-        // If there are no cTokens in circulation, return an exchange rate of 1:1.
-        if (totalSupply == 0) return BASE_UNIT;
+        uint256 cTokenSupply = totalSupply;
 
-        // TODO: Optimize double SLOAD of totalSupply here?
-        return underlying.balanceOf(address(this)).fdiv(totalSupply, BASE_UNIT);
+        if (cTokenSupply == 0) return BASE_UNIT;
+
+        return UNDERLYING.balanceOf(address(this)).fdiv(cTokenSupply, BASE_UNIT);
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                             MOCK FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    function simulateLoss(uint256 underlyingAmount) external {
+        UNDERLYING.safeTransfer(address(0xDEAD), underlyingAmount);
     }
 }
