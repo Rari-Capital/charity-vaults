a8998b90869bbd29088668facea28b4860755074
Andreas Bigger <abigger87@gmail.com>
2021-11-09 09:24:12 -0800

    simple mock strategy

---
+++ src/tests/mocks/SimpleCharityVaultStrategy.sol
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.9;
+
+import {ERC20} from "solmate/tokens/ERC20.sol";
+import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
+import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
+
+import {ERC20Strategy} from "vaults/interfaces/Strategy.sol";
+
+/// @title SimpleCharityVaultStrategy
+/// @notice This is essentially a malicious strategy that over-reports a user's balance
+contract SimpleCharityVaultStrategy is
+    ERC20("CV Simple Mock Strategy", "cvsSMOCK", 18),
+    ERC20Strategy
+{
+    using SafeTransferLib for ERC20;
+    using FixedPointMathLib for uint256;
+
+    /*///////////////////////////////////////////////////////////////
+                           STRATEGY FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    constructor(ERC20 _UNDERLYING) {
+        UNDERLYING = _UNDERLYING;
+
+        BASE_UNIT = 10**_UNDERLYING.decimals();
+    }
+
+    function isCEther() external pure override returns (bool) {
+        return false;
+    }
+
+    function underlying() external view override returns (ERC20) {
+        return UNDERLYING;
+    }
+
+    function mint(uint256 amount) external override returns (uint256) {
+        _mint(msg.sender, amount.fdiv(exchangeRate(), BASE_UNIT));
+
+        UNDERLYING.safeTransferFrom(msg.sender, address(this), amount);
+
+        return 0;
+    }
+
+    function redeemUnderlying(uint256 amount)
+        external
+        override
+        returns (uint256)
+    {
+        _burn(msg.sender, amount.fdiv(exchangeRate(), BASE_UNIT));
+
+        UNDERLYING.safeTransfer(msg.sender, amount + 1);
+
+        return 0;
+    }
+
+    function balanceOfUnderlying(address user)
+        external
+        view
+        override
+        returns (uint256)
+    {
+        return balanceOf[user].fmul(exchangeRate(), BASE_UNIT);
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                             INTERNAL LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    ERC20 internal immutable UNDERLYING;
+
+    uint256 internal immutable BASE_UNIT;
+
+    function exchangeRate() internal view returns (uint256) {
+        uint256 cTokenSupply = totalSupply;
+
+        if (cTokenSupply == 0) return BASE_UNIT;
+
+        return
+            UNDERLYING.balanceOf(address(this)).fdiv(cTokenSupply, BASE_UNIT);
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                             MOCK FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    function simulateLoss(uint256 underlyingAmount) external {
+        UNDERLYING.safeTransfer(address(0xDEAD), underlyingAmount);
+    }
+}
