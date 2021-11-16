a0352a8c4efd1a736527d496af2fc4a66e078346
David Lucid <david@rari.capital>
2021-11-16 13:25:28 -0600

    Update CharityVault.sol

--- src/CharityVault.sol
+++ src/CharityVault.sol
@@ -354,11 +354,11 @@ contract CharityVault is ERC20, Auth {
 
     // Returns the exchange rate of rcvTokens in terms of rvTokens since the last extraction.
     function rcvRvExchangeRateAtLastExtraction() public view returns (uint256) {
-        // If there are no rvTokens in circulation, return an exchange rate of 1:1.
+        // If there are no rcvTokens in circulation, return an exchange rate of 1:1.
         if (totalSupply == 0) return BASE_UNIT;
 
         // TODO: Optimize double SLOAD of totalSupply here?
-        // Calculate the exchange rate by diving the total holdings by the rvToken supply.
+        // Calculate the exchange rate by diving the total holdings by the rcvToken supply.
         return
             rvTokensOwnedByUsersAtLastExtraction().fdiv(totalSupply, BASE_UNIT);
     }
